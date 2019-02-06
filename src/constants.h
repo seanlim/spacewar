81c2a62c9d0a51196792ccfe93c000d9359f93e1
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2019-02-06 13:37:43 +0800

    Update constants

---
+++ src/constants.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#define marginX 480
+#define rows 7
+#define columns 10
+#define tileSize 32
+
+// Image resources
+const char BACKGROUND[] = "assets\\bg.png";
+const char HURRICANE[] = "assets\\hurricane.png";
+const char PLANET[] = "assets\\planet.png";
+const char SHIPS[] = "assets\\ships.png";
+const char UFO[] = "assets\\ufo.png";
+
+const unsigned int BACKGROUND_COLS = 1;
+const unsigned int BACKGROUND_WIDTH = 1024;
+const unsigned int BACKGROUND_HEIGHT = 1024;
+
+const unsigned int HURRICANE_COLS = 1;
+const unsigned int HURRICANE_WIDTH = 200;
+const unsigned int HURRICANE_HEIGHT = 200;
+
+const unsigned int PLANET_COLS = ;
+const unsigned int PLANET_WIDTH = 1000;
+const unsigned int PLANET_HEIGHT = 1000;
+
+const unsigned int SHIPS_COLS = 10;
+const unsigned int SHIPS_WIDTH = 66;
+const unsigned int SHIPS_HEIGHT = 64;
+
+const unsigned int UFO_COLS = 4;
+const unsigned int UFO_WIDTH = 74;
+const unsigned int UFO_HEIGHT = 74;
